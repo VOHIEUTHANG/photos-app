ddf677d7bf3960e35639764d20852af5bf04f402
Vo Hieu Thang <77570864+VOHIEUTHANG@users.noreply.github.com>
2022-06-30 22:18:11 +0700

    try to create api with firebase

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React, { Suspense } from "react";
 import "./App.scss";
-
+import { getFirestore, collection, getDocs } from "firebase/firestore";
 import {
   BrowserRouter as Router,
   Route,
@@ -10,6 +10,14 @@ import {
 } from "react-router-dom";
 import NotFound from "./components/NotFound";
 import Header from "./components/Header";
+import { app, db } from "./firebase";
+
+const photosCol = collection(db, "photos");
+(async () => {
+  const photoSnapshot = await getDocs(photosCol);
+  const photoList = photoSnapshot.docs.map((doc) => doc.data());
+  console.log("🚀 ~ file: index.ts ~ line 21 ~ snapshot", photoList);
+})();
 
 const Photo = React.lazy(() => import("./features/Photo"));
 
@@ -22,7 +30,7 @@ function App() {
             <Header />
             <Routes>
               <Route path="/" element={<Navigate to="/photos" replace />} />
-              <Route path="/photos/*" element={<Photo />} />
+              <Route path="photos/*" element={<Photo />} />
               <Route path="*" element={<NotFound />} />
             </Routes>
           </div>
